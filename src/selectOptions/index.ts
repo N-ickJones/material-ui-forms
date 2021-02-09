fd97a43ac5da862720a03b6b92d1bcfd4afacaab
N-ickJones <armyjonesn@gmail.com>
2021-02-09 12:07:11 -0600

    added select options

---
+++ src/selectOptions/index.ts
@@ -0,0 +1,20 @@
+import { acceptOptions, agreeOptions, availabilityOptions, consentOptions, haveOptions, maritalOptions, phoneOptions, referenceOptions, referralSourceOptions, residenceOptions, signitureOptions, usCityOptions, usStateOptions, workLengthOptions, workScheduleOptions, yesOptions } from "./options";
+
+export {
+    availabilityOptions,
+    maritalOptions,
+    phoneOptions,
+    residenceOptions,
+    workLengthOptions,
+    workScheduleOptions,
+    referenceOptions,
+    referralSourceOptions,
+    consentOptions,
+    haveOptions,
+    agreeOptions,
+    acceptOptions,
+    signitureOptions,
+    yesOptions,
+    usStateOptions,
+    usCityOptions,
+}
