d3b5f01b420e9a78d153de5f6ce01f1e206824ee
N-ickJones <armyjonesn@gmail.com>
2021-02-10 11:08:33 -0600

    added next router to dependency / dev dependencies and fixed useWarnIfUnsavedChanges hook, updated Readme

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# material-ui-hooks
+# material-ui-forms
 
-### Documentation coming soon
+## Author: Nicholas Jones
 
-#### App is still in development
+#### App is still in development. 
+    It's not recommended to start using this package until it's first stable release.
+
+#### Current Issues
+    Need to decouple next router from package
+    Need to add documentation to modules and readme
+    Remove unused event in FormView then modify tsconfig.json
--- src/hooks/useWarnIfUnsavedChanges.tsx
+++ src/hooks/useWarnIfUnsavedChanges.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from 'react';
+import Router from 'next/router';
+
+//TODO Need to decouple the router to allow disconntect next and allow multiple routers
 
 export const useWarnIfUnsavedChanges = (unsavedChanges: boolean) => {
   const message = 'You currently have unsaved changes. Are you sure you want to leave before saving?';
 
   useEffect(() => {
-    /*
     const routeChangeStart = (url: string) => {
       if (Router.asPath !== url && unsavedChanges && !confirm(message)) {
         Router.events.emit('routeChangeError');
@@ -12,7 +14,6 @@ export const useWarnIfUnsavedChanges = (unsavedChanges: boolean) => {
         //throw 'Abort route change. Please ignore this error.';
       }
     };
-    */
 
     const beforeUnload = (e: any) => {
       if (unsavedChanges) {
@@ -24,11 +25,11 @@ export const useWarnIfUnsavedChanges = (unsavedChanges: boolean) => {
     };
 
     window.addEventListener('beforeunload', beforeUnload);
-    //Router.events.on('routeChangeStart', routeChangeStart);
+    Router.events.on('routeChangeStart', routeChangeStart);
 
     return () => {
       window.removeEventListener('beforeunload', beforeUnload);
-      //Router.events.off('routeChangeStart', routeChangeStart);
+      Router.events.off('routeChangeStart', routeChangeStart);
     };
   }, [unsavedChanges]);
 };
